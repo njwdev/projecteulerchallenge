c96d9defd20f63f565a90a7608b3eb685375b951
njwdev <nwwebd@gmail.com>
2020-01-13 01:29:57 +0100

    challenge 4

---
+++ Challenge4/script.js
@@ -0,0 +1,24 @@
+function largestPalindromeProduct(n) {
+  //Sets n digit min
+  let loopStart = Math.pow(10, n - 1);
+  //Sets n digit max
+  let loopEnd = Math.pow(10, n) - 1;
+
+  let arr = [];
+
+  for (let i = loopStart; i <= loopEnd; i++) {
+    for (let j = loopStart; j <= loopEnd; j++) {
+      let sum = i * j;
+      let numstring = sum
+        .toString()
+        .split('')
+        .reverse()
+        .join('');
+      let reverseNum = parseInt(numstring);
+      if (sum === reverseNum && sum > Math.max(...arr)) arr.push(sum);
+    }
+  }
+  return Math.max(...arr);
+}
+
+console.log(largestPalindromeProduct(3));
